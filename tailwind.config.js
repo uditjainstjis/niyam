1719f169c27d7490515b0b2895601d1581082695
Gaurang Khator <gaurangkhator04@gmail.com>
2025-10-31 20:58:37 +0530

    feat: added theme toggle

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./src/app/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
